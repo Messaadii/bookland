0f7596104a6fb6c8c218963b29c7f867a9baee51
Messaadii <alii.messaadi@gmail.com>
2021-12-23 21:04:24 +0100

    fix

--- src/main/java/com/vermeg/bookland/controllers/AdminController.java
+++ src/main/java/com/vermeg/bookland/controllers/AdminController.java
@@ -1,15 +1,31 @@
 package com.vermeg.bookland.controllers;
 
+import com.vermeg.bookland.dtos.BookVenteStatDto;
+import com.vermeg.bookland.dtos.ChiffreAffaireBookDto;
+import com.vermeg.bookland.repositories.CommandeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
 
+    private final CommandeRepository commandeRepository;
+
+    @Autowired
+    public AdminController(CommandeRepository commandeRepository) {
+        this.commandeRepository = commandeRepository;
+    }
+
     @GetMapping("/home")
-    public String home(){
-        return "stat/stat";
+    public String home(Model model){
+        return "redirect:/stat/stat";
     }
 }
--- src/main/java/com/vermeg/bookland/controllers/StatController.java
+++ src/main/java/com/vermeg/bookland/controllers/StatController.java
@@ -18,37 +18,33 @@ import com.vermeg.bookland.dtos.ChiffreAffaireBookDto;
 import com.vermeg.bookland.repositories.CommandeRepository;
 
 
-
-
 @Controller
 @RequestMapping("/stat/")
 public class StatController {
-	private final CommandeRepository commandeRepository;
-	
-	 @Autowired
-	 public StatController(CommandeRepository commandeRepository) {
-	 this.commandeRepository=commandeRepository;
-	 }
-	@GetMapping("/stat")
-	public String barGraph(Model model)
-	{
-	Map<String,Integer> map=new LinkedHashMap<>();
-	List<BookVenteStatDto> list=this.commandeRepository.findTotalVenteBybook();
-	Map<String,Integer> mapchif=new LinkedHashMap<>();
-	List<ChiffreAffaireBookDto> listchif=this.commandeRepository.findChiffreAffaireYear();
-	for(int i=0;i<list.size();i++)
-	{
-		map.put(list.get(i).getTitle(), list.get(i).gettotalVenteBook());
-	}
-	for(int i=0;i<listchif.size();i++)
-	{
-		mapchif.put(listchif.get(i).getDate(), listchif.get(i).getTotal());
-	}
-	
-	System.out.println(mapchif);
-	model.addAttribute("map",map);
-	model.addAttribute("mapchif",mapchif);
-	return "stat/statistic";
+    private final CommandeRepository commandeRepository;
 
-	}
+    @Autowired
+    public StatController(CommandeRepository commandeRepository) {
+        this.commandeRepository = commandeRepository;
+    }
+
+    @GetMapping("/stat")
+    public String barGraph(Model model) {
+        Map<String, Integer> map = new LinkedHashMap<>();
+        List<BookVenteStatDto> list = this.commandeRepository.findTotalVenteBybook();
+        Map<String, Integer> mapchif = new LinkedHashMap<>();
+        List<ChiffreAffaireBookDto> listchif = this.commandeRepository.findChiffreAffaireYear();
+        for (int i = 0; i < list.size(); i++) {
+            map.put(list.get(i).getTitle(), list.get(i).gettotalVenteBook());
+        }
+        for (int i = 0; i < listchif.size(); i++) {
+            mapchif.put(listchif.get(i).getDate(), listchif.get(i).getTotal());
+        }
+
+        System.out.println(mapchif);
+        model.addAttribute("map", map);
+        model.addAttribute("mapchif", mapchif);
+        return "stat/statistic";
+
+    }
 }
